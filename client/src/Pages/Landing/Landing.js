5a49d3b452720ef1fa819ef3c8112377753eae6c
darienmiller88 <darienmiller88@yahoo.com>
2022-04-25 15:07:04 -0400

    retrieving users from landing page

--- client/src/Pages/Landing/Landing.js
+++ client/src/Pages/Landing/Landing.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import Navbar from '../../Components/Navbar/Navbar'
 import Hero from "../../Containers/Hero/Hero"
 import About from '../../Containers/About/About'
@@ -6,8 +6,14 @@ import Benefits from '../../Containers/Benefits/Benefits'
 import Services from "../../Containers/Services/Services"
 import Footer from "../../Containers/Footer/Footer"
 import "./Landing.scss"
+import { base } from "../../Components/BaseUrl/BaseUrl"
+import axios from "axios"
 
 export default function Landing() {
+    useEffect(() => {
+        axios.get(`${base}/api/v1/users`).then(res => console.log(res.data))
+    }, [])
+    
 
     return (
         <>
