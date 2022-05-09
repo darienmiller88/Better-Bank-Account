683737c76de5403de9ec43636b43a2fe55a77f13
darienmiller88 <darienmiller88@yahoo.com>
2022-05-08 22:20:56 -0400

    added cookie domain again

--- api/controllers/UserController.go
+++ api/controllers/UserController.go
@@ -170,7 +170,7 @@ func setCookie(user models.User, res http.ResponseWriter, req *http.Request) {
 		Expires:  expiry,
 		// SameSite: http.SameSiteStrictMode,
 		Secure:   true,
-		Domain: "netlify",
+		Domain: "https://www.netlify.com/",
 	}
 
 	//better-bank-account-api.herokuapp.com
